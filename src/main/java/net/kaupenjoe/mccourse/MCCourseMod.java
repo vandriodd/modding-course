28bfc08068921cb612dfd913cc7831b59b085663
vandriodd <ludasaadi@gmail.com>
2026-06-24 19:37:44 +0400

    Initial commit, Item added

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -1,52 +1,10 @@
 package net.kaupenjoe.mccourse;
 
 import com.mojang.logging.LogUtils;
-import net.kaupenjoe.mccourse.block.ModBlocks;
-import net.kaupenjoe.mccourse.block.entity.ModBlockEntities;
-import net.kaupenjoe.mccourse.effect.ModEffects;
-import net.kaupenjoe.mccourse.enchantment.ModEnchantments;
-import net.kaupenjoe.mccourse.entity.ModEntities;
-import net.kaupenjoe.mccourse.entity.client.MagicProjectileRenderer;
-import net.kaupenjoe.mccourse.entity.client.ModBoatRenderer;
-import net.kaupenjoe.mccourse.entity.client.RhinoRenderer;
-import net.kaupenjoe.mccourse.fluid.ModFluidTypes;
-import net.kaupenjoe.mccourse.fluid.ModFluids;
-import net.kaupenjoe.mccourse.item.ModCreativeModeTabs;
-import net.kaupenjoe.mccourse.item.ModItemProperties;
 import net.kaupenjoe.mccourse.item.ModItems;
-import net.kaupenjoe.mccourse.loot.ModLootModifiers;
-import net.kaupenjoe.mccourse.painting.ModPaintings;
-import net.kaupenjoe.mccourse.particle.ModParticles;
-import net.kaupenjoe.mccourse.potion.BetterBrewingRecipe;
-import net.kaupenjoe.mccourse.potion.ModPotions;
-import net.kaupenjoe.mccourse.recipe.KaupenFurnaceRecipe;
-import net.kaupenjoe.mccourse.recipe.ModRecipes;
-import net.kaupenjoe.mccourse.screen.GemEmpoweringStationScreen;
-import net.kaupenjoe.mccourse.screen.KaupenFurnaceScreen;
-import net.kaupenjoe.mccourse.screen.ModMenuTypes;
-import net.kaupenjoe.mccourse.sound.ModSounds;
-import net.kaupenjoe.mccourse.util.ModWoodTypes;
-import net.kaupenjoe.mccourse.villager.ModVillagers;
-import net.kaupenjoe.mccourse.worldgen.biome.ModTerraBlenderAPI;
-import net.kaupenjoe.mccourse.worldgen.biome.surface.ModSurfaceRules;
-import net.kaupenjoe.mccourse.worldgen.tree.ModFoliagePlacerTypes;
-import net.kaupenjoe.mccourse.worldgen.tree.ModTrunkPlacerTypes;
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.client.renderer.entity.ThrownItemRenderer;
-import net.minecraft.core.Registry;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.alchemy.Potions;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.ComposterBlock;
-import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -56,49 +14,18 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import terrablender.api.SurfaceRuleManager;
-
-import java.awt.*;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MCCourseMod.MOD_ID)
 public class MCCourseMod {
     public static final String MOD_ID = "mccourse";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public MCCourseMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModCreativeModeTabs.register(modEventBus);
-
+        // Register the item into the game
         ModItems.register(modEventBus);
-        ModBlocks.register(modEventBus);
-
-        ModEnchantments.register(modEventBus);
-        ModSounds.register(modEventBus);
-
-        ModLootModifiers.register(modEventBus);
-        ModPaintings.register(modEventBus);
-
-        ModEffects.register(modEventBus);
-        ModPotions.register(modEventBus);
-
-        ModVillagers.register(modEventBus);
-        ModParticles.register(modEventBus);
-
-        ModFluidTypes.register(modEventBus);
-        ModFluids.register(modEventBus);
-
-        ModBlockEntities.register(modEventBus);
-        ModMenuTypes.register(modEventBus);
-
-        ModRecipes.register(modEventBus);
-        ModEntities.register(modEventBus);
-
-        ModTrunkPlacerTypes.register(modEventBus);
-        ModFoliagePlacerTypes.register(modEventBus);
-
-        ModTerraBlenderAPI.registerRegions();
 
         modEventBus.addListener(this::commonSetup);
 
@@ -107,27 +34,14 @@ public class MCCourseMod {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            ComposterBlock.COMPOSTABLES.put(ModItems.KOHLRABI.get(), 0.35f);
-            ComposterBlock.COMPOSTABLES.put(ModItems.KOHLRABI_SEEDS.get(), 0.20f);
 
-            ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(ModBlocks.SNAPDRAGON.getId(), ModBlocks.POTTED_SNAPDRAGON);
-
-            BrewingRecipeRegistry.addRecipe(new BetterBrewingRecipe(Potions.AWKWARD, Items.SLIME_BALL, ModPotions.SLIMEY_POTION.get()));
-
-            SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, MOD_ID, ModSurfaceRules.makeRules());
-        });
     }
 
+    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
-        }
-
-        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
-            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         }
     }
 
@@ -142,24 +56,7 @@ public class MCCourseMod {
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            event.enqueueWork(() -> {
-                Sheets.addWoodType(ModWoodTypes.WALNUT);
 
-                ModItemProperties.addCustomItemProperties();
-
-                ItemBlockRenderTypes.setRenderLayer(ModFluids.SOURCE_SOAP_WATER.get(), RenderType.translucent());
-                ItemBlockRenderTypes.setRenderLayer(ModFluids.FLOWING_SOAP_WATER.get(), RenderType.translucent());
-
-                MenuScreens.register(ModMenuTypes.GEM_EMPOWERING_MENU.get(), GemEmpoweringStationScreen::new);
-                MenuScreens.register(ModMenuTypes.KAUPEN_FURNACE_MENU.get(), KaupenFurnaceScreen::new);
-
-                EntityRenderers.register(ModEntities.RHINO.get(), RhinoRenderer::new);
-                EntityRenderers.register(ModEntities.DICE_PROJECTILE.get(), ThrownItemRenderer::new);
-                EntityRenderers.register(ModEntities.MAGIC_PROJECTILE.get(), MagicProjectileRenderer::new);
-
-                EntityRenderers.register(ModEntities.MOD_BOAT.get(), pContext -> new ModBoatRenderer(pContext, false));
-                EntityRenderers.register(ModEntities.MOD_CHEST_BOAT.get(), pContext -> new ModBoatRenderer(pContext, true));
-            });
         }
     }
 }
--- src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
+++ src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
@@ -1,15 +1,7 @@
 package net.kaupenjoe.mccourse.item;
 
 import net.kaupenjoe.mccourse.MCCourseMod;
-import net.kaupenjoe.mccourse.block.ModBlocks;
-import net.kaupenjoe.mccourse.entity.ModEntities;
-import net.kaupenjoe.mccourse.entity.custom.ModBoatEntity;
-import net.kaupenjoe.mccourse.fluid.ModFluids;
-import net.kaupenjoe.mccourse.item.custom.*;
-import net.kaupenjoe.mccourse.sound.ModSounds;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.*;
-import net.minecraftforge.common.ForgeSpawnEggItem;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,91 +11,13 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MCCourseMod.MOD_ID);
 
+    // Registry object to actually register the item
     public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
             () -> new Item(new Item.Properties()));
+
+    // There's another
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
-
-    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
-            () -> new MetalDetectorItem(new Item.Properties().durability(512)));
-
-    public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI)));
-
-    public static final RegistryObject<Item> PEAT_BRICK = ITEMS.register("peat_brick",
-            () -> new FuelItem(new Item.Properties(), 200));
-
-    public static final RegistryObject<Item> ALEXANDRITE_SWORD = ITEMS.register("alexandrite_sword",
-            () -> new SlowingSwordItem(ModToolTiers.ALEXANDRITE, 2, 3, new Item.Properties().durability(256)));
-    public static final RegistryObject<Item> ALEXANDRITE_PICKAXE = ITEMS.register("alexandrite_pickaxe",
-            () -> new PickaxeItem(ModToolTiers.ALEXANDRITE, 1, 2, new Item.Properties().durability(256)));
-    public static final RegistryObject<Item> ALEXANDRITE_SHOVEL = ITEMS.register("alexandrite_shovel",
-            () -> new ShovelItem(ModToolTiers.ALEXANDRITE, 2, 3, new Item.Properties().durability(256)));
-    public static final RegistryObject<Item> ALEXANDRITE_AXE = ITEMS.register("alexandrite_axe",
-            () -> new AxeItem(ModToolTiers.ALEXANDRITE, 2, 3, new Item.Properties().durability(256)));
-    public static final RegistryObject<Item> ALEXANDRITE_HOE = ITEMS.register("alexandrite_hoe",
-            () -> new HoeItem(ModToolTiers.ALEXANDRITE, 2, 3, new Item.Properties().durability(256)));
-
-    public static final RegistryObject<Item> ALEXANDRITE_PAXEL = ITEMS.register("alexandrite_paxel",
-            () -> new PaxelItem(ModToolTiers.ALEXANDRITE, 2, 3, new Item.Properties().durability(256)));
-    public static final RegistryObject<Item> ALEXANDRITE_HAMMER = ITEMS.register("alexandrite_hammer",
-            () -> new HammerItem(ModToolTiers.ALEXANDRITE, 2, 3, new Item.Properties().durability(256)));
-
-    public static final RegistryObject<Item> ALEXANDRITE_HELMET = ITEMS.register("alexandrite_helmet",
-            () -> new ModArmorItem(ModArmorMaterials.ALEXANDRITE, ArmorItem.Type.HELMET, new Item.Properties()));
-    public static final RegistryObject<Item> ALEXANDRITE_CHESTPLATE = ITEMS.register("alexandrite_chestplate",
-            () -> new ModArmorItem(ModArmorMaterials.ALEXANDRITE, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
-    public static final RegistryObject<Item> ALEXANDRITE_LEGGING = ITEMS.register("alexandrite_leggings",
-            () -> new ModArmorItem(ModArmorMaterials.ALEXANDRITE, ArmorItem.Type.LEGGINGS, new Item.Properties()));
-    public static final RegistryObject<Item> ALEXANDRITE_BOOTS = ITEMS.register("alexandrite_boots",
-            () -> new ModArmorItem(ModArmorMaterials.ALEXANDRITE, ArmorItem.Type.BOOTS, new Item.Properties()));
-
-    public static final RegistryObject<Item> ALEXANDRITE_HORSE_ARMOR = ITEMS.register("alexandrite_horse_armor",
-            () -> new HorseArmorItem(12, new ResourceLocation(MCCourseMod.MOD_ID, "textures/entity/horse/armor/horse_armor_alexandrite.png"), new Item.Properties()));
-
-    public static final RegistryObject<Item> DATA_TABLET = ITEMS.register("data_tablet",
-            () -> new DataTabletItem(new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> KOHLRABI_SEEDS = ITEMS.register("kohlrabi_seeds",
-            () -> new ItemNameBlockItem(ModBlocks.KOHLRABI_CROP.get(), new Item.Properties()));
-
-    public static final RegistryObject<Item> BAR_BRAWL_RECORD = ITEMS.register("bar_brawl_record",
-            () -> new RecordItem(4, ModSounds.BAR_BRAWL, new Item.Properties().stacksTo(1), 2440));
-
-    public static final RegistryObject<Item> RADIATION_STAFF = ITEMS.register("radiation_staff",
-            () -> new RadiationStaffItem(new Item.Properties().durability(1024)));
-
-    public static final RegistryObject<Item> ALEXANDRITE_BOW = ITEMS.register("alexandrite_bow",
-            () -> new BowItem(new Item.Properties().durability(500)));
-    public static final RegistryObject<Item> ALEXANDRITE_SHIELD = ITEMS.register("alexandrite_shield",
-            () -> new ShieldItem(new Item.Properties().durability(500)));
-
-    public static final RegistryObject<Item> SOAP_WATER_BUCKET = ITEMS.register("soap_water_bucket",
-            () -> new BucketItem(ModFluids.SOURCE_SOAP_WATER, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1)));
-
-
-    public static final RegistryObject<Item> WALNUT_SIGN = ITEMS.register("walnut_sign",
-            () -> new SignItem(new Item.Properties().stacksTo(16), ModBlocks.WALNUT_SIGN.get(), ModBlocks.WALNUT_WALL_SIGN.get()));
-    public static final RegistryObject<Item> WALNUT_HANGING_SIGN = ITEMS.register("walnut_hanging_sign",
-            () -> new HangingSignItem(ModBlocks.WALNUT_HANGING_SIGN.get(), ModBlocks.WALNUT_WALL_HANGING_SIGN.get(), 
-                    new Item.Properties().stacksTo(16)));
-
-    public static final RegistryObject<Item> RHINO_SPAWN_EGG = ITEMS.register("rhino_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntities.RHINO, 0x7e9680, 0xc5d1c5,
-                    new Item.Properties()));
-
-    public static final RegistryObject<Item> DICE = ITEMS.register("dice",
-            () -> new DiceItem(new Item.Properties()));
-
-    public static final RegistryObject<Item> WALNUT_BOAT = ITEMS.register("walnut_boat",
-            () -> new ModBoatItem(false, ModBoatEntity.Type.WALNUT, new Item.Properties()));
-    public static final RegistryObject<Item> WALNUT_CHEST_BOAT = ITEMS.register("walnut_chest_boat",
-            () -> new ModBoatItem(true, ModBoatEntity.Type.WALNUT, new Item.Properties()));
-
-    public static final RegistryObject<Item> CATTAIL = ITEMS.register("cattail",
-            () -> new ModWaxingItem(new Item.Properties()));
-    public static final RegistryObject<Item> CATTAIL_SEEDS = ITEMS.register("cattail_seeds",
-            () -> new ItemNameBlockItem(ModBlocks.CATTAIL_CROP.get(), new Item.Properties()));
-
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
