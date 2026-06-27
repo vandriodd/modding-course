53712d727150d20400a22509b7b61636659f5297
vandriodd <ludasaadi@gmail.com>
2026-06-27 21:59:21 +0400

    add custom tooltips

--- src/main/java/net/kaupenjoe/mccourse/block/custom/SoundBlock.java
+++ src/main/java/net/kaupenjoe/mccourse/block/custom/SoundBlock.java
@@ -1,19 +1,23 @@
 package net.kaupenjoe.mccourse.block.custom;
 
-import net.kaupenjoe.mccourse.MCCourseMod;
 import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.Interaction;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Properties pProperties) {
@@ -53,6 +57,12 @@ public class SoundBlock extends Block {
         }
 
         return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.translatable("tooltip.mccourse.sound_block"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 
     // Plays sound when player steps on block, either server or client, doesn't matter
